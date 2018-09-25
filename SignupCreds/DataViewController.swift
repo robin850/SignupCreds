4f3598ce4ceac87b87a74260e2308b27d9b6ad2f
Robin Dupret <robin.dupret@gmail.com>
2018-09-25 11:01:25 +0200

    Initial Commit

---
+++ SignupCreds/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  SignupCreds
+//
+//  Created by Robin Dupret on 25/09/2018.
+//  Copyright © 2018 Sathoro. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
