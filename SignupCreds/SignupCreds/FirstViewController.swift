5833555e9b526427340b2720dbb3bb9b96ec2aeb
Robin Dupret <robin.dupret@gmail.com>
2018-09-27 10:44:10 +0200

    Ajout du parsage JSON basique

--- SignupCreds/SignupCreds/FirstViewController.swift
+++ SignupCreds/SignupCreds/FirstViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    var service : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonPath = Bundle.main.url(forResource: "service", withExtension: "json")
+        service = 0
+        
+        do {
+            let data = try Data(contentsOf: jsonPath!)
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            
+            let jsonServices = json as! [String : Any]
+            let services     = jsonServices["services"] as! Array<[String: Any]>
+            
+            let dataService = services[service!]
+            
+            for element in dataService["elements"] as! Array<[String : Any]> {
+                print(element["section"] as! String)
+                print(element["type"] as! String)
+                
+                let values = element["value"] as! Array<String>
+                for valeur in values {
+                    print(valeur)
+                }
+                
+                print("---------------------")
+            }
+        } catch {
+            print(error)
+        }
     }
-
-
 }
 
