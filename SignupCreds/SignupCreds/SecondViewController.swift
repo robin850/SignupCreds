9ad7f9bf3a159e6afa1fe6bf9ae03ea93b3a0ba4
Samuel <tehsams@gmail.com>
2018-09-28 17:28:30 +0200

    Quelques MaJ mineures

--- SignupCreds/SignupCreds/SecondViewController.swift
+++ SignupCreds/SignupCreds/SecondViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var alertButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appleBlue = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
+        self.alertButton.backgroundColor = .clear
+        self.alertButton.layer.cornerRadius = 15
+        self.alertButton.layer.borderWidth = 1
+        self.alertButton.layer.borderColor = appleBlue.cgColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- SignupCreds/SignupCreds/ThirdViewController.swift
+++ SignupCreds/SignupCreds/ThirdViewController.swift
@@ -9,8 +9,32 @@
 import UIKit
 
 class ThirdViewController : UIViewController {
+    
+    @IBOutlet weak var spotifyService: UIImageView!
+    @IBOutlet weak var netflixService: UIImageView!
+    @IBOutlet weak var alertButton: UIButton!
+    let appleBlue = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.alertButton.backgroundColor = .clear
+        self.alertButton.layer.cornerRadius = 15
+        self.alertButton.layer.borderWidth = 1
+        self.alertButton.layer.borderColor = appleBlue.cgColor
+        
+        self.netflixService.layer.shadowColor = UIColor.black.cgColor
+        self.netflixService.layer.shadowOpacity = 0.1
+        self.netflixService.layer.shadowRadius = 15
+        self.netflixService.layer.shadowPath = UIBezierPath(rect: netflixService.bounds).cgPath
+        self.netflixService.layer.shouldRasterize = false
+        
+        self.spotifyService.layer.shadowColor = UIColor.black.cgColor
+        self.spotifyService.layer.shadowOpacity = 0.1
+        self.spotifyService.layer.shadowRadius = 15
+        self.spotifyService.layer.shadowPath = UIBezierPath(rect: spotifyService.bounds).cgPath
+        self.spotifyService.layer.shouldRasterize = false
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 }
