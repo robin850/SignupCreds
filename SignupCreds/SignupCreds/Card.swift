ba0e807f6166165f6d799fdad41532294b81f580
Robin Dupret <robin.dupret@gmail.com>
2018-11-12 18:47:16 +0100

    Import some parts of the Card pod

--- SignupCreds/SignupCreds/Card.swift
+++ SignupCreds/SignupCreds/Card.swift
@@ -5,5 +5,207 @@
 //  Created by Robin Dupret on 12/11/2018.
 //  Copyright © 2018 Sathoro. All rights reserved.
 //
+import UIKit
 
-import Foundation
+@objc public protocol CardDelegate {
+
+    @objc optional func cardDidTapInside(card: Card)
+    @objc optional func cardWillShowDetailView(card: Card)
+    @objc optional func cardDidShowDetailView(card: Card)
+    @objc optional func cardWillCloseDetailView(card: Card)
+    @objc optional func cardDidCloseDetailView(card: Card)
+    @objc optional func cardIsShowingDetail(card: Card)
+    @objc optional func cardIsHidingDetail(card: Card)
+    @objc optional func cardDetailIsScrolling(card: Card)
+
+    @objc optional func cardHighlightDidTapButton(card: CardHighlight, button: UIButton)
+}
+
+@IBDesignable open class Card: UIView, CardDelegate {
+
+    // Storyboard Inspectable vars
+    /**
+     Color for the card's labels.
+     */
+    @IBInspectable public var textColor: UIColor = UIColor.black
+    /**
+     Amount of blur for the card's shadow.
+     */
+    @IBInspectable public var shadowBlur: CGFloat = 14 {
+        didSet{
+            self.layer.shadowRadius = shadowBlur
+        }
+    }
+    /**
+     Alpha of the card's shadow.
+     */
+    @IBInspectable public var shadowOpacity: Float = 0.6 {
+        didSet{
+            self.layer.shadowOpacity = shadowOpacity
+        }
+    }
+    /**
+     Color of the card's shadow.
+     */
+    @IBInspectable public var shadowColor: UIColor = UIColor.gray {
+        didSet{
+            self.layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    /**
+     The image to display in the background.
+     */
+    @IBInspectable public var backgroundImage: UIImage? {
+        didSet{
+            self.backgroundIV.image = backgroundImage
+        }
+    }
+    /**
+     Corner radius of the card.
+     */
+    @IBInspectable public var cardRadius: CGFloat = 20{
+        didSet{
+            self.layer.cornerRadius = cardRadius
+        }
+    }
+    /**
+     Insets between card's content and edges ( in percentage )
+     */
+    @IBInspectable public var contentInset: CGFloat = 6 {
+        didSet {
+            insets = LayoutHelper(rect: originalFrame).X(contentInset)
+        }
+    }
+    /**
+     Color of the card's background.
+     */
+    override open var backgroundColor: UIColor? {
+        didSet(new) {
+            if let color = new { backgroundIV.backgroundColor = color }
+            if backgroundColor != UIColor.clear { backgroundColor = UIColor.clear }
+        }
+    }
+
+    /**
+     Event handler au moment du clique sur l'élément
+     */
+    @IBInspectable public var onClick : (() -> Void)! = {}
+
+    /**
+     contentViewController  -> The view controller to present when the card is tapped
+     from                   -> Your current ViewController (self)
+     */
+    public func shouldPresent( _ contentViewController: UIViewController?, from superVC: UIViewController?, fullscreen: Bool = false) {
+        if let content = contentViewController {
+            self.superVC = superVC
+            detailVC.addChild(content)
+            detailVC.detailView = content.view
+            detailVC.card = self
+            detailVC.delegate = self.delegate
+            detailVC.isFullscreen = fullscreen
+        }
+    }
+
+    /**
+     Delegate for the card. Should extend your VC with CardDelegate.
+     */
+    public var delegate: CardDelegate?
+
+    //Private Vars
+    fileprivate var tap = UITapGestureRecognizer()
+    fileprivate var detailVC = DetailViewController()
+    weak var superVC: UIViewController?
+    var originalFrame = CGRect.zero
+    public var backgroundIV = UIImageView()
+    public var insets = CGFloat()
+    var isPresenting = false
+
+    //MARK: - View Life Cycle
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+
+    open func initialize() {
+
+        // Tap gesture init
+        self.addGestureRecognizer(tap)
+        tap.delegate = self
+        tap.cancelsTouchesInView = false
+
+        //detailVC.transitioningDelegate = self
+
+        // Adding Subviews
+        self.addSubview(backgroundIV)
+
+        backgroundIV.isUserInteractionEnabled = true
+
+        if backgroundIV.backgroundColor == nil {
+            backgroundIV.backgroundColor = UIColor.white
+            super.backgroundColor = UIColor.clear
+        }
+    }
+
+    override open func draw(_ rect: CGRect) {
+        super.draw(rect)
+        originalFrame = rect
+
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOffset = CGSize.zero
+        self.layer.shadowRadius = shadowBlur
+        self.layer.cornerRadius = cardRadius
+
+        backgroundIV.image = backgroundImage
+        backgroundIV.layer.cornerRadius = self.layer.cornerRadius
+        backgroundIV.clipsToBounds = true
+        backgroundIV.contentMode = .scaleAspectFill
+
+        backgroundIV.frame.origin = bounds.origin
+        backgroundIV.frame.size = CGSize(width: bounds.width, height: bounds.height)
+        contentInset = 6
+    }
+
+
+    //MARK: - Layout
+
+    open func layout(animating: Bool = true){ }
+
+
+    //MARK: - Actions
+
+    @objc func cardTapped() {
+        self.onClick()
+    }
+
+}
+
+//MARK: - Gesture Delegate
+
+extension Card: UIGestureRecognizerDelegate {
+
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        cardTapped()
+    }
+}
+
+
+//MARK: - Helpers
+
+extension UILabel {
+
+    func lineHeight(_ height: CGFloat) {
+
+        let attributedString = NSMutableAttributedString(string: self.text!)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = height
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+        self.attributedText = attributedString
+    }
+}
--- SignupCreds/SignupCreds/CardHighlight.swift
+++ SignupCreds/SignupCreds/CardHighlight.swift
@@ -6,4 +6,161 @@
 //  Copyright © 2018 Sathoro. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable open class CardHighlight: Card {
+
+    /**
+     Text of the title label.
+     */
+    @IBInspectable public var title: String = "welcome \nto \ncards !" {
+        didSet{
+            titleLbl.text = title.uppercased()
+            titleLbl.lineHeight(0.70)
+        }
+    }
+    /**
+     Max font size the title label.
+     */
+    @IBInspectable public var titleSize:CGFloat = 26
+
+    /**
+     Image displayed in the icon ImageView.
+     */
+    @IBInspectable public var icon: UIImage? {
+        didSet{
+            iconIV.image = icon
+            bgIconIV.image = icon
+        }
+    }
+    /**
+     Corner radius for the icon ImageView
+     */
+    @IBInspectable public var iconRadius: CGFloat = 16 {
+        didSet{
+            iconIV.layer.cornerRadius = iconRadius
+            bgIconIV.layer.cornerRadius = iconRadius*2
+        }
+    }
+    /**
+     Text for the card's button.
+     */
+    @IBInspectable public var buttonText: String = "view" {
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
+
+    //Priv Vars
+    private var iconIV = UIImageView()
+    private var actionBtn = UIButton()
+    private var titleLbl = UILabel ()
+    private var lightColor = UIColor(red: 239/255, green: 239/255, blue: 244/255, alpha: 1)
+    private var bgIconIV = UIImageView()
+
+    fileprivate var btnWidth = CGFloat()
+
+    // View Life Cycle
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+
+    override open func initialize() {
+        super.initialize()
+
+        actionBtn.addTarget(self, action: #selector(buttonTapped), for: UIControl.Event.touchUpInside)
+
+        backgroundIV.addSubview(iconIV)
+        backgroundIV.addSubview(titleLbl)
+        backgroundIV.addSubview(actionBtn)
+
+        if backgroundImage == nil {  backgroundIV.addSubview(bgIconIV); }
+        else { bgIconIV.alpha = 0 }
+    }
+
+
+    override open func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        //Draw
+        bgIconIV.image = icon
+        bgIconIV.alpha = backgroundImage != nil ? 0 : 0.6
+        bgIconIV.clipsToBounds = true
+
+        iconIV.image = icon
+        iconIV.clipsToBounds = true
+
+        titleLbl.text = title.uppercased()
+        titleLbl.textColor = textColor
+        titleLbl.font = UIFont.systemFont(ofSize: titleSize, weight: .heavy)
+        titleLbl.adjustsFontSizeToFitWidth = true
+        titleLbl.lineHeight(0.70)
+        titleLbl.minimumScaleFactor = 0.1
+        titleLbl.lineBreakMode = .byTruncatingTail
+        titleLbl.numberOfLines = 3
+        backgroundIV.bringSubviewToFront(titleLbl)
+
+        actionBtn.backgroundColor = UIColor.clear
+        actionBtn.layer.backgroundColor = lightColor.cgColor
+        actionBtn.clipsToBounds = true
+        let btnTitle = NSAttributedString(string: buttonText.uppercased(), attributes: [ NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .black), NSAttributedString.Key.foregroundColor : self.tintColor])
+        actionBtn.setAttributedTitle(btnTitle, for: .normal)
+
+        btnWidth = CGFloat((buttonText.count + 2) * 10)
+
+        layout()
+
+    }
+
+    override open func layout(animating: Bool = true) {
+        super.layout(animating: animating)
+
+        let gimme = LayoutHelper(rect: backgroundIV.frame)
+
+        iconIV.frame = CGRect(x: insets,
+                              y: insets,
+                              width: gimme.Y(25),
+                              height: gimme.Y(25))
+
+        titleLbl.frame.origin = CGPoint(x: insets, y: gimme.Y(5, from: iconIV))
+        titleLbl.frame.size.width = (originalFrame.width * 0.65) + ((backgroundIV.bounds.width - originalFrame.width)/3)
+        titleLbl.frame.size.height = gimme.Y(35)
+
+        bgIconIV.transform = CGAffineTransform.identity
+
+
+        iconIV.layer.cornerRadius = iconRadius
+
+        bgIconIV.frame.size = CGSize(width: iconIV.bounds.width * 2, height: iconIV.bounds.width * 2)
+        bgIconIV.frame.origin = CGPoint(x: gimme.RevX(0, width: bgIconIV.frame.width) + LayoutHelper.Width(40, of: bgIconIV) , y: 0)
+
+
+        bgIconIV.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi/6))
+        bgIconIV.layer.cornerRadius = iconRadius * 2
+
+        actionBtn.frame = CGRect(x: gimme.RevX(0, width: btnWidth) - insets,
+                                 y: gimme.RevY(0, height: 32) - insets,
+                                 width: btnWidth,
+                                 height: 32)
+        actionBtn.layer.cornerRadius = actionBtn.layer.bounds.height/2
+    }
+
+    //Actions
+
+    @objc  func buttonTapped(){
+        UIView.animate(withDuration: 0.2, animations: {
+            self.actionBtn.transform = CGAffineTransform(scaleX: 0.90, y: 0.90)
+        }) { _ in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.actionBtn.transform = CGAffineTransform.identity
+            })
+        }
+        delegate?.cardHighlightDidTapButton?(card: self, button: actionBtn)
+    }
+}
