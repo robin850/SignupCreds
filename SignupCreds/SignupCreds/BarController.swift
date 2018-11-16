robin850/SignupCreds

commit 38c4506ea892e67bd2d1d56237826f44040f2ce3

Réagencement du storyboard et de l'application
en fonction du cahier des charges

SignupCreds/SignupCreds/BarController.swift
@@ -12,7 +12,7 @@ import UIKit
 class BarController : UITabBarController {
     public var service : Int? {
         didSet {
-            let controller = self.viewControllers![0] as! FirstViewController
+            let controller = self.viewControllers![0] as! FormViewController
 
             self.selectedIndex = 0
             controller.generateForm(service: self.service!)

SignupCreds/SignupCreds.xcodeproj/project.pbxproj
@@ -11,13 +11,13 @@
 		008415AE2199EFDD00CE9E6D /* CardHighlight.swift in Sources */ = {isa = PBXBuildFile; fileRef = 008415AD2199EFDD00CE9E6D /* CardHighlight.swift */; };
 		008415B22199F01400CE9E6D /* LayoutHelper.swift in Sources */ = {isa = PBXBuildFile; fileRef = 008415B12199F01400CE9E6D /* LayoutHelper.swift */; };
 		00CD7D33215CBE9200A966EB /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D32215CBE9200A966EB /* AppDelegate.swift */; };
-		00CD7D35215CBE9200A966EB /* FirstViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D34215CBE9200A966EB /* FirstViewController.swift */; };
-		00CD7D37215CBE9200A966EB /* SecondViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D36215CBE9200A966EB /* SecondViewController.swift */; };
+		00CD7D35215CBE9200A966EB /* FormViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D34215CBE9200A966EB /* FormViewController.swift */; };
+		00CD7D37215CBE9200A966EB /* ResultsViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D36215CBE9200A966EB /* ResultsViewController.swift */; };
 		00CD7D3A215CBE9200A966EB /* Main.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = 00CD7D38215CBE9200A966EB /* Main.storyboard */; };
 		00CD7D3C215CBE9400A966EB /* Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 00CD7D3B215CBE9400A966EB /* Assets.xcassets */; };
 		00CD7D3F215CBE9400A966EB /* LaunchScreen.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = 00CD7D3D215CBE9400A966EB /* LaunchScreen.storyboard */; };
-		00CD7D49215CC12400A966EB /* FourthViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D48215CC12400A966EB /* FourthViewController.swift */; };
-		00CD7D4B215CC17100A966EB /* ThirdViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D4A215CC17100A966EB /* ThirdViewController.swift */; };
+		00CD7D49215CC12400A966EB /* AboutViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D48215CC12400A966EB /* AboutViewController.swift */; };
+		00CD7D4B215CC17100A966EB /* ServicesViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00CD7D4A215CC17100A966EB /* ServicesViewController.swift */; };
 		00CD7D4F215CCA5D00A966EB /* service.json in Resources */ = {isa = PBXBuildFile; fileRef = 00CD7D4E215CCA5D00A966EB /* service.json */; };
 		00FA8B362199ABBB00C58948 /* BaseController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00FA8B352199ABBB00C58948 /* BaseController.swift */; };
 		00FA8B382199CB4E00C58948 /* BarController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 00FA8B372199CB4E00C58948 /* BarController.swift */; };
@@ -29,14 +29,14 @@
 		008415B12199F01400CE9E6D /* LayoutHelper.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LayoutHelper.swift; sourceTree = "<group>"; };
 		00CD7D2F215CBE9200A966EB /* SignupCreds.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = SignupCreds.app; sourceTree = BUILT_PRODUCTS_DIR; };
 		00CD7D32215CBE9200A966EB /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
-		00CD7D34215CBE9200A966EB /* FirstViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = FirstViewController.swift; sourceTree = "<group>"; };
-		00CD7D36215CBE9200A966EB /* SecondViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SecondViewController.swift; sourceTree = "<group>"; };
+		00CD7D34215CBE9200A966EB /* FormViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = FormViewController.swift; sourceTree = "<group>"; };
+		00CD7D36215CBE9200A966EB /* ResultsViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ResultsViewController.swift; sourceTree = "<group>"; };
 		00CD7D39215CBE9200A966EB /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
 		00CD7D3B215CBE9400A966EB /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
 		00CD7D3E215CBE9400A966EB /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/LaunchScreen.storyboard; sourceTree = "<group>"; };
 		00CD7D40215CBE9400A966EB /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
-		00CD7D48215CC12400A966EB /* FourthViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = FourthViewController.swift; sourceTree = "<group>"; };
-		00CD7D4A215CC17100A966EB /* ThirdViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ThirdViewController.swift; sourceTree = "<group>"; };
+		00CD7D48215CC12400A966EB /* AboutViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AboutViewController.swift; sourceTree = "<group>"; };
+		00CD7D4A215CC17100A966EB /* ServicesViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ServicesViewController.swift; sourceTree = "<group>"; };
 		00CD7D4E215CCA5D00A966EB /* service.json */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.json; path = service.json; sourceTree = "<group>"; };
 		00FA8B352199ABBB00C58948 /* BaseController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BaseController.swift; sourceTree = "<group>"; };
 		00FA8B372199CB4E00C58948 /* BarController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BarController.swift; sourceTree = "<group>"; };
@@ -72,17 +72,17 @@
 		00CD7D31215CBE9200A966EB /* SignupCreds */ = {
 			isa = PBXGroup;
 			children = (
-				00FA8B372199CB4E00C58948 /* BarController.swift */,
+				00CD7D38215CBE9200A966EB /* Main.storyboard */,
+				00CD7D3D215CBE9400A966EB /* LaunchScreen.storyboard */,
 				00CD7D4E215CCA5D00A966EB /* service.json */,
 				00CD7D32215CBE9200A966EB /* AppDelegate.swift */,
-				00CD7D34215CBE9200A966EB /* FirstViewController.swift */,
-				00CD7D36215CBE9200A966EB /* SecondViewController.swift */,
-				00CD7D38215CBE9200A966EB /* Main.storyboard */,
-				00CD7D48215CC12400A966EB /* FourthViewController.swift */,
-				00CD7D4A215CC17100A966EB /* ThirdViewController.swift */,
+				00CD7D4A215CC17100A966EB /* ServicesViewController.swift */,
+				00CD7D34215CBE9200A966EB /* FormViewController.swift */,
+				00CD7D36215CBE9200A966EB /* ResultsViewController.swift */,
+				00CD7D48215CC12400A966EB /* AboutViewController.swift */,
 				00CD7D3B215CBE9400A966EB /* Assets.xcassets */,
-				00CD7D3D215CBE9400A966EB /* LaunchScreen.storyboard */,
 				00CD7D40215CBE9400A966EB /* Info.plist */,
+				00FA8B372199CB4E00C58948 /* BarController.swift */,
 				00FA8B352199ABBB00C58948 /* BaseController.swift */,
 				008415AB2199EF9800CE9E6D /* Card.swift */,
 				008415AD2199EFDD00CE9E6D /* CardHighlight.swift */,
@@ -163,15 +163,15 @@
 			isa = PBXSourcesBuildPhase;
 			buildActionMask = 2147483647;
 			files = (
-				00CD7D37215CBE9200A966EB /* SecondViewController.swift in Sources */,
+				00CD7D37215CBE9200A966EB /* ResultsViewController.swift in Sources */,
 				008415B22199F01400CE9E6D /* LayoutHelper.swift in Sources */,
 				008415AE2199EFDD00CE9E6D /* CardHighlight.swift in Sources */,
 				00CD7D33215CBE9200A966EB /* AppDelegate.swift in Sources */,
-				00CD7D4B215CC17100A966EB /* ThirdViewController.swift in Sources */,
-				00CD7D35215CBE9200A966EB /* FirstViewController.swift in Sources */,
+				00CD7D4B215CC17100A966EB /* ServicesViewController.swift in Sources */,
+				00CD7D35215CBE9200A966EB /* FormViewController.swift in Sources */,
 				00FA8B362199ABBB00C58948 /* BaseController.swift in Sources */,
 				00FA8B382199CB4E00C58948 /* BarController.swift in Sources */,
-				00CD7D49215CC12400A966EB /* FourthViewController.swift in Sources */,
+				00CD7D49215CC12400A966EB /* AboutViewController.swift in Sources */,
 				008415AC2199EF9800CE9E6D /* Card.swift in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;

SignupCreds/SignupCreds/AboutViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FourthViewController : UIViewController, BaseController {
+class AboutViewController : UIViewController, BaseController {
     
     @IBOutlet weak var alertButton: UIButton!
     

SignupCreds/SignupCreds/Base.lproj/Main.storyboard
@@ -12,7 +12,7 @@
         <!--Formulaire-->
         <scene sceneID="hNz-n2-bh7">
             <objects>
-                <viewController id="9pv-A4-QxB" customClass="FirstViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
+                <viewController id="9pv-A4-QxB" customClass="FormViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
                     <view key="view" contentMode="scaleToFill" id="tsR-hK-woN" userLabel="FormView">
                         <rect key="frame" x="0.0" y="0.0" width="375" height="1000"/>
                         <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
@@ -85,12 +85,12 @@
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="W5J-7L-Pyd" sceneMemberID="firstResponder"/>
             </objects>
-            <point key="canvasLocation" x="749.60000000000002" y="-744.82758620689663"/>
+            <point key="canvasLocation" x="-838" y="1063"/>
         </scene>
         <!--Résultats-->
         <scene sceneID="wg7-f3-ORb">
             <objects>
-                <viewController id="8rJ-Kc-sve" customClass="SecondViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
+                <viewController id="8rJ-Kc-sve" customClass="ResultsViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
                     <view key="view" contentMode="scaleToFill" id="QS5-Rx-YEW" userLabel="ResultsView">
                         <rect key="frame" x="0.0" y="0.0" width="375" height="667"/>
                         <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
@@ -129,12 +129,12 @@
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="4Nw-L8-lE0" sceneMemberID="firstResponder"/>
             </objects>
-            <point key="canvasLocation" x="750" y="360"/>
+            <point key="canvasLocation" x="-1" y="913"/>
         </scene>
         <!--Services-->
         <scene sceneID="FxH-l8-25e">
             <objects>
-                <viewController storyboardIdentifier="Services" useStoryboardIdentifierAsRestorationIdentifier="YES" id="7jo-kO-rwt" customClass="ThirdViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
+                <viewController storyboardIdentifier="Services" useStoryboardIdentifierAsRestorationIdentifier="YES" id="7jo-kO-rwt" customClass="ServicesViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
                     <view key="view" contentMode="scaleToFill" id="h2e-H4-GYQ">
                         <rect key="frame" x="0.0" y="0.0" width="375" height="1000"/>
                         <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
@@ -197,12 +197,12 @@
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="8ja-9E-eLd" userLabel="First Responder" sceneMemberID="firstResponder"/>
             </objects>
-            <point key="canvasLocation" x="749.60000000000002" y="1265.6671664167918"/>
+            <point key="canvasLocation" x="-1665" y="1062"/>
         </scene>
         <!--À propos-->
         <scene sceneID="eZg-rJ-VrX">
             <objects>
-                <viewController id="WgD-cx-60m" customClass="FourthViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
+                <viewController id="WgD-cx-60m" customClass="AboutViewController" customModule="SignupCreds" customModuleProvider="target" sceneMemberID="viewController">
                     <view key="view" contentMode="scaleToFill" id="oqa-70-YQx">
                         <rect key="frame" x="0.0" y="0.0" width="375" height="667"/>
                         <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
@@ -249,7 +249,7 @@ Robin Dupret</string>
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="3na-gv-ovE" userLabel="First Responder" sceneMemberID="firstResponder"/>
             </objects>
-            <point key="canvasLocation" x="750" y="2175"/>
+            <point key="canvasLocation" x="850" y="913"/>
         </scene>
         <!--Bar Controller-->
         <scene sceneID="yl2-sM-qoP">
@@ -261,15 +261,15 @@ Robin Dupret</string>
                         <color key="backgroundColor" red="0.0" green="0.0" blue="0.0" alpha="0.0" colorSpace="custom" customColorSpace="sRGB"/>
                     </tabBar>
                     <connections>
+                        <segue destination="7jo-kO-rwt" kind="relationship" relationship="viewControllers" id="iVV-aD-5QZ"/>
                         <segue destination="9pv-A4-QxB" kind="relationship" relationship="viewControllers" id="u7Y-xg-7CH"/>
                         <segue destination="8rJ-Kc-sve" kind="relationship" relationship="viewControllers" id="lzU-1b-eKA"/>
-                        <segue destination="7jo-kO-rwt" kind="relationship" relationship="viewControllers" id="iVV-aD-5QZ"/>
                         <segue destination="WgD-cx-60m" kind="relationship" relationship="viewControllers" id="zDM-h2-ycC"/>
                     </connections>
                 </tabBarController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="HuB-VB-40B" sceneMemberID="firstResponder"/>
             </objects>
-            <point key="canvasLocation" x="0.0" y="0.0"/>
+            <point key="canvasLocation" x="-406" y="-2"/>
         </scene>
     </scenes>
     <resources>

SignupCreds/SignupCreds/FormViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, BaseController {
+class FormViewController: UIViewController, BaseController {
     //var service : Int?
     
     @IBOutlet weak var alertButton: UIButton!
@@ -93,7 +93,7 @@ class FirstViewController: UIViewController, BaseController {
         let button = UIButton(frame: CGRect(x: 16, y: y, width: self.scrollView.frame.width - 16, height: 50))
         button.setTitle("Enregistrer", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
-        button.addTarget(self, action: #selector(FirstViewController.buttonAction(_:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(FormViewController.buttonAction(_:)), for: .touchUpInside)
         self.scrollView.addSubview(button)
     }
     

SignupCreds/SignupCreds/ResultsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController : UIViewController, BaseController {
+class ResultsViewController : UIViewController, BaseController {
     @IBOutlet weak var alertButton : UIButton!
 
     override func viewDidLoad() {

SignupCreds/SignupCreds/ServicesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ThirdViewController : UIViewController, BaseController {
+class ServicesViewController : UIViewController, BaseController {
     
     @IBOutlet weak var alertButton: UIButton!
     

commit 694b0c178101ad213adefd93f6860b3ac6b63155

Fix a runtime error when unwrapping the scrollView
Since we are not presenting the controller before trying to add
elements to it, the values are not unwrapped and it produces an
error at runtime.

Regression from 38c4506.

SignupCreds/SignupCreds/BarController.swift
@@ -14,7 +14,7 @@ class BarController : UITabBarController {
         didSet {
             let controller = self.viewControllers![1] as! FormViewController
 
-            self.selectedIndex = 0
+            self.selectedIndex = 1
             controller.generateForm(service: self.service!)
         }
     }