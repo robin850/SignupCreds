de3bccd41d668d3ed27e21d7bad22272fe27a1d2
Samuel <tehsams@gmail.com>
2018-11-30 11:43:05 +0100

    Ajout du CPU Usage dans le popup

--- SignupCreds/BaseController.swift
+++ SignupCreds/BaseController.swift
@@ -52,6 +52,43 @@ class BaseController : UIViewController {
         } else {
             x += "\nMémoire ROM totale : Erreur !"
         }
+    
+        /* Infos CPU */
+        var totalUsageOfCPU: Double = 0.0
+        var threadsList = UnsafeMutablePointer(mutating: [thread_act_t]())
+        var threadsCount = mach_msg_type_number_t(0)
+        let threadsResult = withUnsafeMutablePointer(to: &threadsList) {
+            return $0.withMemoryRebound(to: thread_act_array_t?.self, capacity: 1) {
+                task_threads(mach_task_self_, $0, &threadsCount)
+            }
+        }
+        if threadsResult == KERN_SUCCESS {
+            for index in 0..<threadsCount {
+                var threadInfo = thread_basic_info()
+                var threadInfoCount = mach_msg_type_number_t(THREAD_INFO_MAX)
+                let infoResult = withUnsafeMutablePointer(to: &threadInfo) {
+                    $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                        thread_info(threadsList[Int(index)], thread_flavor_t(THREAD_BASIC_INFO), $0, &threadInfoCount)
+                    }
+                }
+                
+                guard infoResult == KERN_SUCCESS else {
+                    break
+                }
+                
+                let threadBasicInfo = threadInfo as thread_basic_info
+                if threadBasicInfo.flags & TH_FLAGS_IDLE == 0 {
+                    totalUsageOfCPU = (totalUsageOfCPU + (Double(threadBasicInfo.cpu_usage) / Double(TH_USAGE_SCALE) * 100.0))
+                }
+            }
+        }
+        vm_deallocate(mach_task_self_, vm_address_t(UInt(bitPattern: threadsList)), vm_size_t(Int(threadsCount) * MemoryLayout<thread_t>.stride))
+        if(totalUsageOfCPU != 0.0) {
+            x += "\n\nCPU : \(totalUsageOfCPU)%"
+        } else {
+            x += "\n\nCPU : Erreur !"
+        }
+
 
         /* Création du popup */
         let alertController = UIAlertController(title: "Infos Système", message: x, preferredStyle: .alert)
